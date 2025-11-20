8c1bca44f8badb6f50573d5e33bcd9d7bd6849a7
viashchuk <victoriya.dev@icloud.com>
2025-11-20 19:56:13 +0100

    Init

---
+++ task-1/Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Victoria Iashchuk on 20/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ task-1/Calculator/Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Victoria Iashchuk on 20/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
