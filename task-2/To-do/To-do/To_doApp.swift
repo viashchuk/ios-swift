d76d9bf6ed0d2735ec7ba2f07cbc1df04a2bc2f2
viashchuk <victoriya.dev@icloud.com>
2025-12-03 10:46:24 +0100

    Init task 2

---
+++ task-2/To-do/To-do/To_doApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_doApp.swift
+//  To-do
+//
+//  Created by Victoria Iashchuk on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct To_doApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
