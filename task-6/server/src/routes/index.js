4d8aa5b974cab92da6567c5dcf9431307e0af9c3
viashchuk <victoriya.dev@icloud.com>
2026-01-29 22:30:43 +0100

    Refactor

--- task-6/server/src/routes/index.js
+++ task-6/server/src/routes/index.js
@@ -3,13 +3,11 @@ import { Router } from 'express'
 import authRoutes from './authRoutes.js'
 import productRoutes from './productRoutes.js'
 import orderRoutes from './orderRoutes.js'
-import paymentRoutes from './paymentRoutes.js'
 
 const router = Router()
 
 router.use('/api/', authRoutes)
 router.use('/api/', productRoutes)
 router.use('/api/', orderRoutes)
-router.use('/api/', paymentRoutes)
 
 export default router
