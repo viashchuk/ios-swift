dbe2dc677b3ab05bf043e6dcddaf984658c8080a
viashchuk <victoriya.dev@icloud.com>
2026-01-30 23:24:51 +0100

    Fetching products and categories from server application

--- task-4/CoffeeShop/CoffeeShop/Models/Product.swift
+++ task-4/CoffeeShop/CoffeeShop/Models/Product.swift
@@ -7,13 +7,14 @@
 
 import Foundation
 
-struct Product: Codable, Identifiable {
+struct Product: Codable, Identifiable, Hashable {
     let id: Int
     let name: String
     let description: String?
     let price: Double
     let categoryId: String
     let imageUrl: String?
+    let category: Category?
     let createdAt: Date?
     let updatedAt: Date?
 }
